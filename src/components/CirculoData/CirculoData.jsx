40e8a6bc96caf124cbc1bb7e40e9054621eeb25b
FedeColombo <colombofederico17@gmail.com>
2023-04-28 20:11:59 -0300

    se agregaron circulos de estadistica al card de estacion, y algunos estilos mas

---
+++ src/components/CirculoData/CirculoData.jsx
@@ -0,0 +1,38 @@
+import React from 'react'
+import './CirculoData.css'
+
+import { CircularProgressbar, buildStyles } from 'react-circular-progressbar';
+import 'react-circular-progressbar/dist/styles.css';
+
+const CirculoData = ({ col, value, text }) => {
+
+  return (
+    <div className={col}>
+        <CircularProgressbar value={value} maxValue={100} text={`${value} ${text}`} strokeWidth={6}
+        styles={buildStyles({
+            // Rotation of path and trail, in number of turns (0-1)
+            rotation: 0.50,
+        
+            // Whether to use rounded or flat corners on the ends - can use 'butt' or 'round'
+            strokeLinecap: 'round',
+            
+            // Text size
+            textSize: '16px',
+        
+            // How long animation takes to go from one percentage to another, in seconds
+            pathTransitionDuration: 1.8,
+        
+            // Can specify path transition in more detail, or remove it entirely
+            // pathTransition: 'none',
+        
+            // Colors
+            pathColor: `rgba(62, 152, 199)`,
+            textColor: 'rgba(62, 152, 199)',
+            trailColor: '#d6d6d6',
+            backgroundColor: '#3e98c7',
+          })} />
+    </div>
+  );
+};
+
+export default CirculoData;
--- src/components/Estacion/Estacion.jsx
+++ src/components/Estacion/Estacion.jsx
@@ -1,8 +1,20 @@
 import React, { useEffect } from 'react'
 import './Estacion.css'
 import { Link } from 'react-router-dom/cjs/react-router-dom.min'
+import CirculoData from '../CirculoData/CirculoData'
+import { useState } from 'react'
 
 function Estacion({estacion}) {
+
+  const [temperatura, setTemperatura] = useState(0)
+  const [pm5, setPm5] = useState(0)
+  const [pm25, setPm25] = useState(0)
+
+  useEffect(() => {
+    setTemperatura(27)
+    setPm5(82)
+    setPm25(42)
+  },[])
 
   return (
     <Link to={`/estacion/${estacion.id}`} className="col-11 col-md-3 row estacion align">
@@ -11,6 +23,9 @@ function Estacion({estacion}) {
         </div> */}
         <h1 className='title'>{estacion.name}</h1>
         <h1 className='col-12 subtitle'>{estacion.lat}, {estacion.lng}</h1>
+        <CirculoData col={"col-4"} value={temperatura} text="c"/>
+        <CirculoData col={"col-4"} value={pm5} text="pm"/>
+        <CirculoData col={"col-4"} value={pm25} text="pm"/>
     </Link>
   )
 }
