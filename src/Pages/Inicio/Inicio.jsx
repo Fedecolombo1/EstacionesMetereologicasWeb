680714efa3f74771e0dedf6dffe620e38cab0ffb
naachovega <igna.vega@hotmail.com>
2023-05-09 21:09:38 -0300

    Se agrega la pagina de inicio basica

---
+++ src/Pages/Inicio/Inicio.jsx
@@ -0,0 +1,11 @@
+import './Inicio.css'
+
+export default function Inicio() {
+    return (
+        <div className="root-Inicio col-12">
+            <h1 className='title-Inicio'>RespirAr</h1>
+            <h3>Seguimiento de Estaciones Medioambientales</h3>
+            <a href="/Home">¡Descubrilas!</a>
+        </div>
+    )
+}
--- src/routes.jsx
+++ src/routes.jsx
@@ -8,24 +8,28 @@ import Home from './Pages/Home/Home'
 import MisEstaciones from './Pages/MisEstaciones/MisEstaciones'
 import Detalle from "./Pages/Detalle/Detalle";
 import AboutUs from "./Pages/AboutUs/AboutUs";
+import Inicio from "./Pages/Inicio/Inicio";
 
 export default function Routes() {
   return (
     <Router>
-        <Switch>
-          <Route exact path="/">
-            <Home />
-          </Route>        
-          <Route exact path="/MisEstaciones">
-            <MisEstaciones />
-          </Route>
-          <Route exact path="/AboutUs">
-            <AboutUs />
-          </Route>        
-        </Switch>
-        <Route path="/estacion/:id">
-            <Detalle />
+      <Switch>
+        <Route exact path="/">
+          <Inicio />
         </Route>
+        <Route exact path="/Home">
+          <Home />
+        </Route>
+        <Route exact path="/MisEstaciones">
+          <MisEstaciones />
+        </Route>
+        <Route exact path="/AboutUs">
+          <AboutUs />
+        </Route>
+      </Switch>
+      <Route path="/estacion/:id">
+        <Detalle />
+      </Route>
     </Router>
   );
 }
