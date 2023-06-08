ad94b4e68e9e4003726585bade10ae5ee0140e2d
naachovega <igna.vega@hotmail.com>
2023-06-08 20:22:03 -0300

    Se agrego la capa de servicio con los endpoints a donde se apuntara al backend

---
+++ src/Services/Estaciones/index.js
@@ -0,0 +1,17 @@
+const getEstaciones = () => {
+
+}
+
+const getEstacionById = (id) => {
+    
+}
+
+const getHistoricalData = (id,attribute) => {
+
+}
+const getFilteredHistoricalData = (id,attribute, minDate,maxDate) => {
+
+}
+
+
+export default {getEstaciones, getEstacionById}
---
+++ src/Services/Export/index.js
@@ -0,0 +1,5 @@
+const exportToCSV = (data) => {
+
+}
+
+export default exportToCSV
